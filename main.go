214bff192f5398d71fabe561a0e91437b208ef11
Mabud <pavelalam401@gmail.com>
2023-09-28 01:23:09 +0530

    cleanups++

--- main.go
+++ main.go
@@ -26,62 +26,7 @@ type FirebaseConfig struct {
 
 func main() {
 
-	// viper.SetConfigFile(".env")
-	// viper.ReadInConfig()
-	// viper.AutomaticEnv()
-
-	// projectID := viper.GetString("FIREBASE_PROJECT_ID")
-	// port := viper.GetString("PORT")
-	// universalDomain := viper.GetString("UNIVERSAL_DOMAIN")
-
-	// privateKeyID := viper.GetString("PRIVATE_KEY_ID")
-	// privateKey := viper.GetString("PRIVATE_KEY")
-	// stype := viper.GetString("TYPE")
-
-	// clientEmail := viper.GetString("CLIENT_EMAIL")
-	// clientID := viper.GetString("CLIENT_ID")
-	// authURI := viper.GetString("AUTH_URI")
-	// tokenURI := viper.GetString("TOKEN_URI")
-	// authProviderX509CertURL := viper.GetString("AUTH_PROVIDER_X509_CERT_URL")
-	// clientX509CertURL := viper.GetString("CLIENT_X509_CERT_URL")
-	// privateKey = strings.Replace(privateKey, "\\n", "\n", -1)
-
-	// fireBaseConfig := FirebaseConfig{
-	// 	Type:                    stype,
-	// 	ProjectID:               projectID,
-	// 	PrivateKeyID:            privateKeyID,
-	// 	PrivateKey:              privateKey,
-	// 	ClientEmail:             clientEmail,
-	// 	ClientID:                clientID,
-	// 	AuthURI:                 authURI,
-	// 	TokenURI:                tokenURI,
-	// 	AuthProviderX509CertURL: authProviderX509CertURL,
-	// 	ClientX509CertURL:       clientX509CertURL,
-	// 	UniversalDomain:         universalDomain,
-	// }
-
-	// // Convert the FirebaseConfig to a JSON string.
-	// fireBaseConfigJSON, err := json.MarshalIndent(fireBaseConfig, "", "  ")
-	// if err != nil {
-	// 	log.Fatalf("Error marshalling FirebaseConfig to JSON: %v", err)
-	// }
-
-	// filePath := "./firebase-config.json"
-	// err = os.WriteFile(filePath, fireBaseConfigJSON, 0644)
-	// if err != nil {
-	// 	log.Fatalf("Error writing JSON file: %v", err)
-	// }
-
-	// fmt.Println("JSON file written:", filePath)
-
-	// fileContent, err := os.ReadFile(filePath)
-	// if err != nil {
-	// 	log.Fatalf("Error reading JSON file: %v", err)
-	// }
-
-	// fmt.Println("JSON file content:")
-	// fmt.Println(string(fileContent))
-
+	
 	r := gin.Default()
 
 	r.GET("/cred", handlers.GetPostingsHandler)
@@ -95,24 +40,7 @@ func main() {
 	r.GET("/syncwithSql", handlers.AllScrapersHandler)
 	r.GET("/getallJobsFromSQL", handlers.GetAllJobsFromSqlite)
 
-	// r.GET("/sync", handlers.SyncAll)
-
-	// r.GET("/getallJobs", handlers.GetJobsFromDB)
-
-	// opt := option.WithCredentialsFile(filePath)
-	// firestoreClient, err := firestore.NewClient(context.Background(), projectID, opt)
-	// if err != nil {
-	// 	log.Fatalf("Error initializing Firestore client: %v", err)
-	// }
-
-	// r.GET("/syncFirestore", func(c *gin.Context) {
-	// 	handlers.SyncWithFireBase(c, firestoreClient)
-	// })
-
-	// r.GET("/getJobsFromFirestore", func(c *gin.Context) {
-	// 	handlers.GetJobsFromFirestore(c, firestoreClient)
-	// })
-
+	
 	r.GET("/", func(c *gin.Context) {
 		c.File("static/base.html")
 	})
