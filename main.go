35c66bc0176107adfd3b3fa32f955b5991c64707
Mabud <pavelalam401@gmail.com>
2023-09-28 01:39:09 +0530

    cleanups++

--- main.go
+++ main.go
@@ -10,19 +10,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type FirebaseConfig struct {
-	Type                    string `json:"type"`
-	ProjectID               string `json:"project_id"`
-	PrivateKeyID            string `json:"private_key_id"`
-	PrivateKey              string `json:"private_key"`
-	ClientEmail             string `json:"client_email"`
-	ClientID                string `json:"client_id"`
-	AuthURI                 string `json:"auth_uri"`
-	TokenURI                string `json:"token_uri"`
-	AuthProviderX509CertURL string `json:"auth_provider_x509_cert_url"`
-	ClientX509CertURL       string `json:"client_x509_cert_url"`
-	UniversalDomain         string `json:"universal_domain"`
-}
 
 func main() {
 
