420d8c1c4732e5a9f2c2817454d0f1afacea06ba
Mabud <pavelalam401@gmail.com>
2023-11-11 01:49:15 +0530

    more refactoring

--- main.go
+++ main.go
@@ -101,7 +101,7 @@ func main() {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://jobs-scraper-production.up.railway.app")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://jobs-scraper-production.up.railway.app/")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
